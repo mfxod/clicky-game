b3d402fc1b56aae58eb7c38a44fa85a1818a0f49
Matt <mfxod828@gmail.com>
2019-09-15 20:14:24 -0400

    building out components

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+
+class Card extends Component {
+    // state = {
+    // }
+
+    // functions
+
+    render() {
+      return (
+        // stuff
+      )
+    }    
+}
+
+export default Card
