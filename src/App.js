115a61821307bfa42396490aad1252378c93ee91
Matt <mfxod828@gmail.com>
2019-09-18 17:06:50 -0400

    shuffle function

--- src/App.js
+++ src/App.js
@@ -7,9 +7,43 @@ import symbols from './symbols.json'
 class App extends Component {
   state = {
     symbols
+    // ,
+    // alreadyClicked = [],
+    // score = 0,
+    // highScore = 0
   }
 
-  // functions
+  // SHUFFLING IMAGES
+  // try to make this a higher order function instead...
+  shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const swapIndex = Math.floor(Math.random() * (i + 1))
+      const currentIndex = array[i]
+      const indexToSwap = array[swapIndex]
+      array[i] = indexToSwap
+      array[swapIndex] = currentIndex
+    }
+    return array
+  } 
+
+  // CHECK IF IMG WAS ALREADY CLICKED
+  alreadyClicked() {
+
+  }
+
+  clickImage = id => {
+    console.log("testing... card clicked: " + id)
+    // if (this.alreadyClicked does contain clicked id) {
+    //   alert somehow
+    //   do high score logic
+    //   reset score
+    // } else {
+    //   add clicked id to alreadyClicked array
+    //   increment score
+    //   setState
+    // }
+    this.setState(this.shuffle(symbols))
+  }
 
   render() {
     return (
@@ -17,6 +51,7 @@ class App extends Component {
         <Score />
         {this.state.symbols.map(symbol => (
           <Card
+            clickImage={this.clickImage}
             id={symbol.id}
             key={symbol.id}
             name={symbol.name}
@@ -27,12 +62,5 @@ class App extends Component {
     )
   }
 }
-
-// starter code
-// function App() {
-//   return (
-//     <Card />
-//   )
-// }
 
 export default App;
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,9 +5,8 @@ function Card(props) {
     return (
       <div className="card mx-auto">
         <div className="img-container mx-auto">
-          <img alt={props.name} src={props.image} />
+          <img alt={props.name} src={props.image} onClick={() => props.clickImage(props.id)}/>
         </div>
-        {/* <span onClick={() => props.removeFriend(props.id)} className="remove"></span> */}
       </div>
     )
 }
